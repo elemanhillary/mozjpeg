80c54feac728193d26a77c72613b29ac88d30737
elemanhillary <elemanhillary@gmail.com>
2022-05-28 00:56:23 +0300

    - generate cmake files

--- toolchain.cmake
+++ toolchain.cmake
@@ -31,3 +31,6 @@ set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/Xcode
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang)
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang)
